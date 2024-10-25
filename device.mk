0abc78c2317a62016cfa6c149f91e467db556241
Ipin 22 <ucihamala573@gmail.com>
2024-10-25 18:12:56 +0000

    CPH2083: import seccomp from stock
    Signed-off-by: ipin22 <ucihamala573@gmail.com>

--- device.mk
+++ device.mk
@@ -76,6 +76,12 @@ PRODUCT_PACKAGES += \
     ueventd.rc \
     ueventd.reserve.rc \
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomptore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policytore@1.1.policy \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
