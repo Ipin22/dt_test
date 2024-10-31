2ff17247343ca3beebfb07b8177f0254207a16c0
Ipin22 <ucihamala573@gmail.com>
2024-11-01 01:14:15 +0700

    CPH2083: fix build error

--- device.mk
+++ device.mk
@@ -117,7 +117,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomptore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policytore@1.1.policy \
+    $(LOCAL_PATH)/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policytore@1.1.policy \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
 
