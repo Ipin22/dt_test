7a492677b7550d650617d286e4280acb92d18834
Ipin22 <ucihamala573@gmail.com>
2024-11-03 18:20:14 +0000

    kontol

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -184,15 +184,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := engineer_vendor_shell.sh
 LOCAL_SRC_FILES := bin/engineer_vendor_shell.sh
 LOCAL_MODULE_CLASS := EXECUTABLES
